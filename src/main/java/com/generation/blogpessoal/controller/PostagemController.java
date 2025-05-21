b16806073155a5b939fbc212307cdf39b6cfd33c
Brassaroto <gubrassarotolira@gmail.com>
2025-05-20 23:45:06 -0300

    Atualizações de código

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,5 +1,5 @@
 package com.generation.blogpessoal.controller;
- 
+
 import java.util.List;
 import java.util.Optional;
 
@@ -20,69 +20,136 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
- 
+
 @RestController
 @RequestMapping("/postagens")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
-	
+
 	@Autowired
 	private PostagemRepository postagemRepository;
-	
+
+	@Autowired
+	private TemaRepository temaRepository;
+
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll() {
-		// SELECT * FROM tb_postagem;
-		return ResponseEntity.ok(postagemRepository.findAll());
-	}
 
+		/**
+		 * O Método executará a consulta: SELECT * FROM tb_postagens;
+		 */
+		return ResponseEntity.ok(postagemRepository.findAll());
+
+	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
-	    return postagemRepository.findById(id)
-	        .map(resposta -> ResponseEntity.ok(resposta))
-	        .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    	
-    }
-	
+
+		/**
+		 * O Método executará a consulta: SELECT * FROM tb_postagens WHERE id = ?; A
+		 * interrogação representa o valor inserido no parâmetro id do método getById
+		 */
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagem>> getAllByTitulo(@PathVariable String titulo) {
+
+		/**
+		 * O Método executará a consulta: SELECT * FROM tb_postagens WHERE titulo LIKE
+		 * '%?%"; A interrogação representa o valor inserido no parâmetro titulo do
+		 * método getAllByTitulo
+		 */
 		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
-	
 
 	}
-	
-	@PostMapping 
-	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
-		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+
+		/** 
+		 * Verifica se o tema existe antes de persistir a postagem no Banco de dados
+		 * */
+		if (temaRepository.existsById(postagem.getTema().getId())) {
+
+			/**
+			 * O Método executará a consulta: INSERT INTO tb_postagens VALUES (titulo,
+			 * texto, data) VALUES (?, ?, ?); As interrogações representam os valores
+			 * inseridos nos respectivos atributos do objeto postagem, parâmetro do método
+			 * post.
+			 */
+			return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		}
 		
+		/** 
+		 * Caso o tema não exista, retorna um Bad Request informando que o tema não existe
+		 * */
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O Tema não existe!", null);
 	}
-	
+
 	@PutMapping
-	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
-		
-		if(postagem.getId() > 0)
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+
+		/**
+		 * Verifica se o id é nulo. Se for nulo, retorna o HTTP Status 400 - BAD_REQUEST
+		 */
+		if (postagem.getId() == null)
 			return ResponseEntity.badRequest().build();
+
+		/**
+		 * Antes de atualizar, verifica se a postagem existe. Se existir, atualiza
+		 */
+		if (postagemRepository.existsById(postagem.getId())) {
+			
+			/** 
+			 * Verifica se o tema existe antes de atualizar a postagem no Banco de dados
+			 * */
+			if (temaRepository.existsById(postagem.getTema().getId()))
+				/**
+				 * O Método executará a consulta: UPDATE tb_postagens SET titulo = ?, texto = ?,
+				 * data = ? WHERE id = ?; As interrogações representam os valores inseridos nos
+				 * respectivos atributos do objeto postagem, parâmetro do método post.
+				 */
+				return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
 		
-		if(postagemRepository.existsById(postagem.getId())) 
-		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
-	
-	    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			/** 
+			 * Caso o tema não exista, retorna um Bad Request informando que o tema não existe
+			 * */
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O Tema não existe!", null);
+			
+		}
+		/**
+		 * Se a postagem não existir, retorna o HTTP Status 404 - NOT_FOUND
+		 */
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
 	}
+
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
-		
+
+		/**
+		 * Busca a postagem pelo id e guarda o resultado no Optional postagem
+		 */
 		Optional<Postagem> postagem = postagemRepository.findById(id);
 
+		/**
+		 * Verifica se o Optional postagem está vazio. Se estiver vazio, retorna o HTTP
+		 * Status 404 - NOT_FOUND
+		 */
 		if (postagem.isEmpty())
-		    throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
+		/**
+		 * Caso contrário, o Método executará a consulta: DELETE FROM tb_postagens WHERE
+		 * id = ?; A interrogação representa parâmetro id do método delete.
+		 */
 		postagemRepository.deleteById(id);
-		
-		// DELETE FROM tb_postagens WHERE id=?;
 
 	}
-
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Postagem;
 
-public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-    List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
-
-    /* SELECT * FROM tb_postagens WHERE titulo LIKE "%?%; */
-
-
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+	/* SELECT * FROM tb_postagens WHERE titulo LIKE "%?%"; */
+			
 }
